f0ec2d4d873443c3b50ae5a64fc53e782351fb0d
SUDHIKSHA-CODES <sudhiksha863@gmail.com>
2026-07-15 20:03:36 +0530

    add random word selection

--- codealpha.py
+++ codealpha.py
@@ -1,1 +1,9 @@
 print("Welcome to Hangman Game!")
+import random
+
+words = ["apple", "tiger", "house", "plant", "chair"]
+
+secret_word = random.choice(words)
+
+print("Welcome to Hangman Game!")
+print("Selected word:", secret_word)   # Remove this later
