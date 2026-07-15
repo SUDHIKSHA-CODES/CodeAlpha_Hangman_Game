5f570583818f5c2ba35050548e3d0d2659639b40
SUDHIKSHA-CODES <sudhiksha863@gmail.com>
2026-07-15 20:11:11 +0530

    Implement game loop for continuous guessing

--- codealpha.py
+++ codealpha.py
@@ -27,3 +27,29 @@ for letter in secret_word:
         display_word += "_ "
 
 print(display_word)
+import random
+
+words = ["apple", "tiger", "house", "plant", "chair"]
+secret_word = random.choice(words)
+
+guessed_letters = []
+
+while True:
+
+    display_word = ""
+
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+
+    print(display_word)
+
+    if "_" not in display_word:
+        print("You Win!")
+        break
+
+    guess = input("Enter a letter: ").lower()
+
+    guessed_letters.append(guess)
